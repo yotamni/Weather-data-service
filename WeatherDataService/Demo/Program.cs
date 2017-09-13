ceba530c7c852f505d410bf558128a049d45d2c9
Yotam Nitzan <yotamni@gmail.com>
2017-09-13 16:51:10 +0300

    initialize commit

---
+++ WeatherDataService/Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherDataService;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            Location location = new Location();
+            using (WebClient client = new WebClient())
+            {
+                string xml = client.DownloadString(Properties.Settings.Default.url + "zip=94040&mode=xml&units=metric&appid=" + Properties.Settings.Default.key);
+            }
+            try
+            {
+                throw new WeatherDataServiceException("test exception");
+            }
+            catch(WeatherDataServiceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ WeatherDataService/WeatherDataService/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WeatherDataService
+{
+    public interface IWeatherDataService
+    {
+        WeatherData GetWeatherData(Location location);
+    }
+
+    public class WeatherData
+    {
+
+    }
+
+    public class WeatherDataServiceFactory: IWeatherDataService
+    {
+        private static WeatherDataServiceFactory instance;
+        private WeatherDataServiceFactory() { }
+        public static WeatherDataServiceFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WeatherDataServiceFactory();
+                }
+                return instance;
+            }
+        }
+
+        public static void test()
+        {
+            Console.WriteLine("In test");
+        }
+
+        public WeatherData GetWeatherData(Location location)
+        {
+            return new WeatherData();
+        }
+    }
+
+    public class Location
+    {
+        string name;
+        int id;
+        int zipCode;
+
+        public Location(string name="",int id=-1,int zipCode=-1)
+        {
+           this.name = name;
+            this.id = id;
+            this.zipCode = zipCode;
+        }
+        
+    }
+
+    public class WeatherDataServiceException: System.Exception
+    {
+        public WeatherDataServiceException() : base()
+        {
+        }
+        public WeatherDataServiceException(String message) : base(message)
+        {
+            Console.WriteLine("test exception in WeatherDataServiceException(String message)");
+        }
+        public WeatherDataServiceException(String message, Exception innerException): base(message, innerException)
+        {
+        }
+        protected WeatherDataServiceException(SerializationInfo info, StreamingContext context): base(info, context)
+        {
+        }
+
+    }
+}
